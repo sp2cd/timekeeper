6641592ead721e777f9dd2bbf6c7b018be5993b9
sp2cd <sp2cd@virginia.edu>
2022-04-30 20:53:46 -0400

    Implement time parsing

--- src/components/TimeSquareForm/TimeSquareForm.tsx
+++ src/components/TimeSquareForm/TimeSquareForm.tsx
@@ -5,19 +5,33 @@ import { StartTimeField } from '../StartTimeField/StartTimeField';
 import { TimeSquareFormButton } from '../TimeSquareFormButton/TimeSquareFormButton';
 import _ from 'lodash';
 import { TimeUtils } from '../../util/TimeUtils';
+import { StringUtils } from '../../util/StringUtils';
 
+export interface TimeSquareFormValues {
+    name: string;
+    startTime: string;
+    elapsedTime: string;
+}
 export interface TimeSquareFormProps {
 
 }
 export function TimeSquareForm({ ...props }: TimeSquareFormProps) {
     const [started, setStarted] = useState<boolean>(false);
-    const formikOnSubmit = (values: any, actions: any) => {
+    const formikOnSubmit = (values: TimeSquareFormValues, actions: any) => {
         setStarted(true);
+        console.log('Starting the count up!', values.startTime);
+        const { hours, minutes } = TimeUtils.parseTimeString(values.startTime)
+        var today = new Date();
+        today.setHours(hours!, minutes!);
+        console.log('DateTime:', today);
     }
 
-    const formikValidate = (values: any) => {
+    const formikValidate = (values: TimeSquareFormValues) => {
         const errorObject = {};
-        if (!isValidTime(_.get(values, 'startTime', ''))) {
+        // if (StringUtils.isBlankString(values.name)) {
+        //     _.set(errorObject, 'name', 'Name is missing!');
+        // }
+        if (!isValidTime(values.startTime)) {
             _.set(errorObject, 'startTime', 'Start time is not a valid time.');
         }
         return errorObject;
@@ -31,7 +45,7 @@ export function TimeSquareForm({ ...props }: TimeSquareFormProps) {
     return (
         <>
             <Formik
-                initialValues={{ name: '', startTime: '', elapsedTime: '' }}
+                initialValues={{ name: '', startTime: '', elapsedTime: '' } as TimeSquareFormValues}
                 onSubmit={formikOnSubmit}
                 validate={formikValidate}
                 validateOnChange
--- src/util/TimeUtils.ts
+++ src/util/TimeUtils.ts
@@ -8,4 +8,40 @@ export namespace TimeUtils {
         var isValid = /^(0*[0-9]|[0-1]?[0-2]):([0-5][0-9])(:[0-5][0-9])*\s*(PM|AM)?$/.test(s);
         return isValid;
     }
+
+    export interface MyTime {
+        hours: number | null;
+        minutes: number | null;
+    }
+    export const parseTimeString = (s: string) => {
+        if (TimeUtils.isValid24HourHHMMSS(s)) {
+            const regexp = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])*?$/;
+            const capturingGroups = s.match(regexp);
+            console.log(capturingGroups);
+            const hours = capturingGroups?.[1];
+            const minutes = capturingGroups?.[2];
+            console.log(hours, 'hours and', minutes, 'minutes');
+            return {
+                hours: +hours!,
+                minutes: +minutes!
+            } as MyTime
+        } else if (TimeUtils.isValid12HourHHMMSS(s)) {
+            const regexp = /^(0*[0-9]|[0-1]?[0-2]):([0-5][0-9])(:[0-5][0-9])*\s*(PM|AM)?$/;
+            const capturingGroups = s.match(regexp);
+            console.log(capturingGroups);
+            const hours = capturingGroups?.[1];
+            const minutes = capturingGroups?.[2];
+            const period = capturingGroups?.[4];
+            console.log(hours, 'hours and', minutes, 'minutes');
+            return {
+                hours: period === 'AM' ? +hours! : +hours! + 12,
+                minutes: +minutes!
+            } as MyTime
+        } else {
+            return {
+                hours: null,
+                minutes: null
+            }
+        }
+    }
 }
